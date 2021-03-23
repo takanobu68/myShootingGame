b32d5ecfbf37ce2a7def193a5e4c412e22ffb885
Takanobu <61861946+takanobu68@users.noreply.github.com>
2021-03-23 23:35:24 +0900

    init関数の設定

--- script/main.js
+++ script/main.js
@@ -1,3 +1,27 @@
-import { GameScreen } from "./module/canvas2d.js";
+import { UtilityCanvas } from "./module/UtilityCanvas.js";
 
-function init() {}
+export function init() {
+  let util, canvas, ctx, image;
+
+  const gameScreen = document.getElementById("game-screen");
+
+  util = new UtilityCanvas(gameScreen);
+  canvas = util.canvas;
+  ctx = util.context;
+
+  util.imageLoader("../images/viper.png", (loadedImage) => {
+    image = loadedImage;
+    initialize();
+    render();
+  });
+
+  function initialize() {
+    canvas.width = gameScreen.clientWidth;
+    canvas.height = gameScreen.clientHeight;
+  }
+
+  function render() {
+    util.drawRect(0, 0, canvas.width, canvas.height, "#eee");
+    ctx.drawImage(image, 100, 100);
+  }
+}
