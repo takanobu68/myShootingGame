dec5a5724b7e7f2e5a08c7e8a9904cc0912bb57e
Takanobu <61861946+takanobu68@users.noreply.github.com>
2021-03-24 02:06:26 +0900

    Playerクラスの作成

--- script/Charactor/Player.js
+++ script/Charactor/Player.js
@@ -1,7 +1,21 @@
 import { CharactorBase } from "../base/CharactorBase.js";
+import { Position } from "../param/Position.js";
 
 export class Player extends CharactorBase {
   constructor(ctx, x, y, image) {
     super(ctx, x, y, 0, image);
+
+    this.isComing = false;
+
+    this.comingStart = null;
+
+    this.comingEndPosition = null;
+  }
+
+  setComing(startX, startY, endX, endY) {
+    this.isComing = true;
+    this.comingStart = Date.now();
+    this.position.set(startX, startY);
+    this.comingEndPosition = new Position(endX, endY);
   }
 }
