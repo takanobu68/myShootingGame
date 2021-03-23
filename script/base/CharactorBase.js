9c89d226eaf5ccfb998aa92a39a6ac1b54ec814a
Takanobu <61861946+takanobu68@users.noreply.github.com>
2021-03-24 01:06:17 +0900

    CharactorBaseクラス作成

---
+++ script/base/CharactorBase.js
@@ -0,0 +1,14 @@
+import { Position } from "../param/Position.js";
+
+export class CharactorBase {
+  constructor(ctx, x, y, life, image) {
+    this.ctx = ctx;
+    this.position = new Position(x, y);
+    this.life = life;
+    this.image = image;
+  }
+
+  draw() {
+    this.ctx.drawImage(this.ctx, this.image, this.position.x, this.position.y);
+  }
+}
