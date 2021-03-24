a5af8c3a1a51e5691ac57fa246d524b9db7e3c83
Takanobu <61861946+takanobu68@users.noreply.github.com>
2021-03-24 20:30:54 +0900

    vectorプロパティと、setVectorメソッドの追加

--- script/Character/Shot.js
+++ script/Character/Shot.js
@@ -1,14 +1,20 @@
 import { CharacterBase } from "../common/CharacterBase.js";
+import { Position } from "../common/Position.js";
 
 export class Shot extends CharacterBase {
   constructor(ctx, x, y, w, h, imagePath) {
     super(ctx, x, y, w, h, 0, imagePath);
     this.speed = 7;
+    this.vector = new Position(0.0, -1.0);
   }
 
   set(x, y) {
     this.position.set(x, y);
     this.life = 1;
+  }
+
+  setVector(x, y) {
+    this.vector.set(x, y);
   }
 
   update() {
@@ -19,7 +25,9 @@ export class Shot extends CharacterBase {
     if (this.position.y + this.height < 0) {
       this.life = 0;
     }
-    this.position.y -= this.speed;
+
+    this.position.x += this.vector.x * this.speed;
+    this.position.y += this.vector.y * this.speed;
 
     this.draw();
   }
