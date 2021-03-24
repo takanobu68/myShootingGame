e5e102c05b08bb379f9e5c2c44019a89b710a1c4
Takanobu <61861946+takanobu68@users.noreply.github.com>
2021-03-24 14:18:30 +0900

    constructorにw,h drawImageメソッドの編集

--- script/common/CharacterBase.js
+++ script/common/CharacterBase.js
@@ -1,14 +1,25 @@
 import { Position } from "./Position.js";
 
 export class CharacterBase {
-  constructor(ctx, x, y, life, image) {
+  constructor(ctx, x, y, w, h, life, image) {
     this.ctx = ctx;
     this.position = new Position(x, y);
+    this.width = w;
+    this.height = h;
     this.life = life;
     this.image = image;
   }
 
   draw() {
-    this.ctx.drawImage(this.image, this.position.x, this.position.y);
+    let offsetX = this.width / 2;
+    let offsetY = this.height / 2;
+
+    this.ctx.drawImage(
+      this.image,
+      this.position.x - offsetX,
+      this.position.y - offsetY,
+      this.width,
+      this.height
+    );
   }
 }
