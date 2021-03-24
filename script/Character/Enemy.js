4ea6d015f0c32ab6bb65551e126c017da4205620
Takanobu <61861946+takanobu68@users.noreply.github.com>
2021-03-25 02:09:22 +0900

    プロパティの追加とsetメソッドの追加編集

--- script/Character/Enemy.js
+++ script/Character/Enemy.js
@@ -4,12 +4,20 @@ export class Enemy extends CharacterBase {
   constructor(ctx, x, y, w, h, imagePath) {
     super(ctx, x, y, w, h, 0, imagePath);
 
+    this.type = "default";
+
+    this.frame = 0;
+
     this.speed = 3;
+
+    this.shotArray = null;
   }
 
-  set(x, y, life = 1) {
+  set(x, y, life = 1, type = "default") {
     this.position.set(x, y);
     this.life = life;
+    this.type = type;
+    this.frame = 0;
   }
 
   update() {
