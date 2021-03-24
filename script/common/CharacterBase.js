e9a1ec69d708ae73f1fe8eb9f2c717255a2bbe78
Takanobu <61861946+takanobu68@users.noreply.github.com>
2021-03-24 12:30:02 +0900

    キャラクター幅の設定

--- script/common/CharacterBase.js
+++ script/common/CharacterBase.js
@@ -1,14 +1,25 @@
 import { Position } from "./Position.js";
 
 export class CharacterBase {
-  constructor(ctx, x, y, life, image) {
+  constructor(ctx, x, y, w, h, life, image) {
     this.ctx = ctx;
     this.position = new Position(x, y);
+    this.w = w;
+    this.h = h;
     this.life = life;
     this.image = image;
   }
 
   draw() {
-    this.ctx.drawImage(this.image, this.position.x, this.position.y);
+    let offSetX = this.w / 2;
+    let offSetY = this.h / 2;
+
+    this.ctx.drawImage(
+      this.image,
+      this.position.x - offSetX,
+      this.position.y - offSetY,
+      this.width,
+      this.height
+    );
   }
 }
