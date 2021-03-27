1663bfb34e94bbbd2e84676f1e1712273b826fc6
Takanobu <61861946+takanobu68@users.noreply.github.com>
2021-03-27 16:49:45 +0900

    updateメソッドの追加

--- script/Character/BackgroundStar.js
+++ script/Character/BackgroundStar.js
@@ -26,7 +26,6 @@ export class BackgroundStar {
      * 座標
      * @type{Vector2}
      */
-
     this.position = null;
   }
 
@@ -35,4 +34,21 @@ export class BackgroundStar {
     this.position = new Vector2(x, y);
   }
 
+  update() {
+    // 色を設定する
+    this.ctx.fillStyle = this.color;
+    // 星の現在位置を速度に応じて動かす
+    this.positiom.y += this.speed;
+    // 星の矩形を描画する
+    this.ctx.fillRect(
+      his.position.x - this.size / 2,
+      this.position.y - this.size / 2,
+      this.size,
+      this.size
+    );
+    // もし画面外に出てしまったら上端部に戻す
+    if (this.position.y + this.size > this.ctx.canvas.height) {
+      this.position.y = -this.size;
+    }
+  }
 }
