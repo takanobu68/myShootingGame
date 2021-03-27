582de9d7e27035a091d4c3d5dce39eef164a807a
Takanobu <61861946+takanobu68@users.noreply.github.com>
2021-03-27 13:48:20 +0900

    ベクトルの長さを返す静的メソッドの追加

--- script/common/Vector2.js
+++ script/common/Vector2.js
@@ -1,4 +1,13 @@
 export class Vector2 {
+  /**
+   * ベクトルの長さを返す静的メソッド
+   * @static
+   * @param {number} x - X 要素
+   * @param {number} y - Y 要素
+   */
+  static calcLength(x, y) {
+    return Math.sqrt(x * x + y * y);
+  }
   constructor(x, y) {
     this.x = x;
     this.y = y;
