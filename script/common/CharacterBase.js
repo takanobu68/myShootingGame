230276e82a88f465e3390c10d0aa67f9ecf6708e
Takanobu <61861946+takanobu68@users.noreply.github.com>
2021-03-27 16:11:27 +0900

    進行方向を角度を元に設定する setVectorFromAngleメソッドを追加した

--- script/common/CharacterBase.js
+++ script/common/CharacterBase.js
@@ -20,6 +20,20 @@ export class CharacterBase {
     this.vector.set(x, y);
   }
 
+  /**
+   * 進行方向を角度を元に設定する
+   * @param {number} angle - 回転量（ラジアン）
+   */
+  setVectorFromAngle(angle) {
+    // 自身の回転量を設定する
+    this.angle = angle;
+    // ラジアンからサインとコサインを求める
+    let sin = Math.sin(angle);
+    let cos = Math.cos(angle);
+    // 自身の vector プロパティに設定する
+    this.vector.set(cos, sin);
+  }
+
   draw() {
     let offsetX = this.width / 2;
     let offsetY = this.height / 2;
