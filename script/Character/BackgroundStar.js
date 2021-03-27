c1d63476dcdf78f70ae8fb9bdd7d2285cb4b7f26
Takanobu <61861946+takanobu68@users.noreply.github.com>
2021-03-27 16:40:37 +0900

    BackgroundStarクラスの作成 constructorとプロパティの追加

---
+++ script/Character/BackgroundStar.js
@@ -0,0 +1,30 @@
+export class BackgroundStar {
+  constructor(ctx, size, speed, color = "#ffffff") {
+    this.ctx = ctx;
+
+    /**
+     * 大きさ
+     * @type{number}
+     */
+    this.size = size;
+
+    /**
+     * 移動速度
+     * @type{number}
+     */
+    this.speed = speed;
+
+    /**
+     * 塗りつぶす色
+     * @type{string}
+     */
+    this.color = color;
+
+    /**
+     * 座標
+     * @type{Vector2}
+     */
+
+    this.position = null;
+  }
+}
